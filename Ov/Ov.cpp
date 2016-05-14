f69186e38d60d27ea0885283100e8cb37c6bb3db
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2016-05-14 17:07:28 +0900

    labeling

--- Ov/Ov.cpp
+++ Ov/Ov.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <opencv2/opencv.hpp> // インクルードファイル指定
 #include <opencv2/opencv_lib.hpp> // 静的リンクライブラリの指定
+#include "Labeling.h"
 
 using namespace std;
 using namespace cv; 
@@ -52,45 +53,110 @@ int contours(Mat binaryImage) {
     return 0;
 }
 
+bool isRedHSV(Mat hsv, int index) {
+    if (hsv.data[index] >= 150 && hsv.data[index] <= 180 && // H
+        hsv.data[index + 1] >= 70 && // S
+        hsv.data[index + 2] >= 70) { // V
+        return true;
+    }
+    return false;
+}
+
+void extractColor(Mat org) {
+    if (org.empty()) throw exception();
+
+    //画像を入れるMatの宣言
+    Mat hsv;    //HSV変換後の画像
+
+    //ラベリング関連の変数
+    LabelingBS labeling;
+    RegionInfoBS *ri;
+
+    //画像サイズの指定
+    const int height = org.rows;
+    const int width = org.cols;
+
+    Mat result(height, width, CV_8UC1);    //青部分を切り出した白黒画像
+    short *dst = new short[width * height];//ラベリング出力先
+
+    result = Mat::zeros(height, width, CV_8UC1);
+
+    //HSV画像への変換
+    //この方法ではHが0-180の範囲で表される
+    cvtColor(org, hsv, CV_BGR2HSV);
+
+    for (int y = 0; y < height;y++) {
+        for (int x = 0; x < width; x++) {
+            int index = hsv.step*y + (x * 3);
+            if (!isRedHSV(hsv, index)) continue;
+
+            //該当色範囲を白くする
+            result.data[result.step*y + x] = 255;            
+        }
+    }
+    //ラベリングの実行
+    labeling.Exec((uchar *)result.data, dst, width, height, true, 30);
+
+    int ltop_x, ltop_y, rbottom_x, rbottom_y;
+
+    for (int i = 0; i < labeling.GetNumOfResultRegions(); i++) {
+        ri = labeling.GetResultRegionInfo(i);
+        ri->GetMin(ltop_x, ltop_y);
+        ri->GetMax(rbottom_x, rbottom_y);
+        cout << i << ", min: (" << ltop_x << ", " << ltop_y << ")" << endl;
+        cout << i << ", max: (" << rbottom_x << ", " << rbottom_y << ")" << endl;
+        rectangle(org, Point(ltop_x, ltop_y), Point(rbottom_x, rbottom_y), Scalar(150, 150, 150));
+    }
+/*
+    //最大の領域を四角で囲む
+    ri = labeling.GetResultRegionInfo(0);
+    int ltop_x, ltop_y, rbottom_x, rbottom_y;
+    ri->GetMin(ltop_x, ltop_y);
+    ri->GetMax(rbottom_x, rbottom_y);
+    rectangle(org, Point(ltop_x, ltop_y), Point(rbottom_x, rbottom_y), Scalar(255, 255, 255));
+*/
+    //画像の表示
+    imshow("Original", org);
+    imshow("Extract", result);
+    waitKey(5000);
+}
+
 int main()
 {
-    try {
-        // 相対パスから指定する方法が分からず。
-        string original_image_path = "C:/Users/eiurur/Documents/Visual Studio 2015/Projects/Ov/x64/Debug/data/original.png";
-        Mat bin_img = binarize(original_image_path);
-        contours(bin_img);
-        string original_rotate_image_path = "C:/Users/eiurur/Documents/Visual Studio 2015/Projects/Ov/x64/Debug/data/original_rotate.png";
-        binarize(original_rotate_image_path);
+    const string FILES_DIR = "C:/Users/eiurur/Documents/Visual Studio 2015/Projects/Ov/x64/Debug/data/";
 
+    // 相対パスから指定する方法が分からず。
+    string original_image_path = FILES_DIR + "original.png";
+    string original_rotate_image_path = FILES_DIR + "original_rotate.png";
+    string original_image_drew_path = FILES_DIR + "original_drew.png";
+    string original_image_drew2_path = FILES_DIR + "original_drew2.png";
+    string test_blur_image_path = FILES_DIR + "test_blue.png";
+
+    //cv::Mat gray_img = cv::imread(test_blur_image_path);
+
+    try {
+ /*       Mat bin_img = binarize(original_image_path);
+        contours(bin_img);
+        binarize(original_rotate_image_path);
+*/
+        // 右半分をトリミング(ROI領域の抽出)
+
+        // 右側の箇所から四角形を抽出し、それぞれclone()
+
+        // 
+
+        // 赤い領域の抽出
+        extractColor(imread(original_image_drew_path));
+
+        // OCR
+
+        // 
     }
     catch (exception e) {
         // 受け取った例外のwhat()を呼び出すことで、エラーメッセージが取得できます。
         cout << e.what() << endl;
         return 1;
     }
-    //width220, height150の画像を作成
-    Mat src = Mat::zeros(150, 220, CV_8UC3);
-
-    //赤色で画像にHello Worldを描く
-    putText(src, "Hello World", Point(5, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 200), 2, CV_AA);
-
-    //緑色で画像に線を描く
-    line(src, Point(190, 25), Point(190, 45), Scalar(0, 200, 0), 3);
-
-    //要素を直接操作し画像に点を描く
-    for (int x = 188; x < 192; x++) {
-        for (int y = 53; y < 57; y++) { // y座標
-            for (int i = 0; i < 2; i++) { // i < 3にするとBGRすべての要素を200にする
-                src.at<uchar>(Point(x * 3 + i, y)) = saturate_cast<uchar>(200);
-            }
-        }
-    }
-
-    //画像を表示
-    imshow("", src);
-
-    //ウインドウの表示時間(0はキーが押されるまで閉じない)
-    waitKey(0);
 
     return 0;
 }
